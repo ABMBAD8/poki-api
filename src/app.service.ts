b430e2046419978e2ee96152691e842b540c0d2a
Moses Bolofo (ZA) <moses.bolofo@absa.africa>
2021-08-09 03:54:17 +0200

    Added description to swagger file and using a modal class for JSon URL

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,15 +1,13 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from './config.service';
+import { PokemonURL } from './_model/Pokemon';
 
 import https = require("https");
 
 @Injectable()
 export class AppService {
 
-  /**
-   *
-   */
   constructor(private _http: HttpService, private _config: ConfigService) {
 
   }
@@ -18,7 +16,7 @@ export class AppService {
     return this._http.get(`${this._config.get('POKEMON_API')}item/?limit=100&offset=0`, { httpsAgent: new https.Agent({ rejectUnauthorized: false }) });
   }
 
-  getPokemon(url: any) {
-    return this._http.get(url, { httpsAgent: new https.Agent({ rejectUnauthorized: false }) });
+  getPokemon(url: PokemonURL) {
+    return this._http.get(url.url, { httpsAgent: new https.Agent({ rejectUnauthorized: false }) });
   }
 }
